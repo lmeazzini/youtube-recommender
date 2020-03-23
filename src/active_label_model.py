0675b682b9b83212c4adbcaba40a47218bb7343a
luis <luis.meazzini@gmail.com>
2020-03-23 19:08:09 -0300

    active learning phase

--- src/active_label_model.py
+++ src/active_label_model.py
@@ -6,8 +6,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import hstack
 
 
-df = pd.read_csv('../data/raw_data.csv')
-df = df[df['y'].notnull()]
+raw_data = pd.read_csv('../data/raw_data.csv')
+df = raw_data[raw_data['y'].notnull()]
 
 df_clean = pd.DataFrame(index=df.index)
 
@@ -43,7 +43,7 @@ features = pd.DataFrame(index=df_clean.index)
 y = df['y'].copy()
 
 # Extracting time since publication feature
-features['time_since_pub'] = (pd.to_datetime("2020-03-15") -  # HARDCODED
+features['time_since_pub'] = (pd.to_datetime("2020-03-24") -  # HARDCODED
                               df_clean['date']) / np.timedelta64(1, 'D')
 
 # Extracting n of view feature
@@ -105,3 +105,62 @@ preds = mdl.predict(X_val_title)
 print('log_loss: ', log_loss(y_val, preds))
 print('avg_precision_score: ', average_precision_score(y_val, val_proba[:, 1]))
 print('roc_auc: ', roc_auc_score(y_val, val_proba[:, 1]))
+
+# Beginning active learning phase
+
+# Reading unlabeled data
+df_u = raw_data[raw_data['y'].isnull()]
+df_u = df_u[df_u['watch-title'].notnull()]
+
+df_clean_u = pd.DataFrame(index=df_u.index)
+df_clean_u['title'] = df_u['watch-title']
+
+# Clean date attribute
+clean_date = df_u['watch-time-text'].str.extract(r"(\d+) de ([a-z]+)\. de (\d+)")
+clean_date[0] = clean_date[0].dropna().astype(str)
+clean_date[1] = clean_date[1].map(month_map)
+clean_date[2] = clean_date[2].dropna().astype(str)
+clean_date = clean_date.dropna().apply(lambda x: " ".join(x), axis=1)
+
+df_clean_u['date'] = pd.to_datetime(clean_date, format="%d %b %Y")
+df_clean_u.dropna(inplace=True)
+df_u = df_u.loc[df_clean_u.index]
+
+# Clean view number
+views = df_u['watch-view-count'].str.extract(r"(\d+\.?\d*)", expand=False)
+df_clean_u['views'] = views.str.replace(".", "").fillna(0).astype(int)
+
+# Making features DataFrame
+features_u = pd.DataFrame(index=df_clean_u.index)
+features_u['time_since_pub'] = (pd.to_datetime("2020-03-24") -  # HARDCODED
+                                df_clean_u['date']) / np.timedelta64(1, 'D')
+
+# Extracting n of view feature
+features_u['views'] = df_clean_u['views']
+
+# Extracting n of view/day feature
+features_u['views_per_day'] = features_u['views'] / features_u['time_since_pub']
+
+# Droping time_since_pub to prevent bias
+features_u = features_u.drop(['time_since_pub'], axis=1)
+
+# Features from title
+title_bow_u = title_vec.transform(df_clean_u['title'])
+X_title_u = hstack([features_u, title_bow_u])
+
+# Predicting the scores
+proba_u = mdl.predict_proba(X_title_u)[:, 1]
+df_u['p'] = proba_u
+
+# Mask thresholds selected by histogram analysis
+mask_u = (df_u['p'] >= 0.35) & (df_u['p'] <= 1.)
+n_hard = mask_u.sum()
+
+# Selecting 100 samples to active learning
+# 71 hard ones, 29 random ones
+hard_ones = df_u[mask_u]
+n_random = 100 - n_hard
+random_ones = df_u[~mask_u].sample(n_random, random_state=42)
+
+active_learning = pd.concat([hard_ones, random_ones])
+active_learning.to_csv("../data/active_label.csv", index=False)
