ca238df5fcb4d9a6f9480928fe1b0766c950cc71
luis <luis.meazzini@gmail.com>
2020-03-27 09:45:47 -0300

    predict api and sql update

--- src/deploy/app.py
+++ src/deploy/app.py
@@ -1,10 +1,9 @@
-import os.path
-from flask import Flask
-import os
-import json
+from flask import Flask, request
 import run_backend as run_backend
-import time
-
+import get_data
+import ml_utils
+import sqlite3 as sql
+import json
 
 app = Flask(__name__)
 
@@ -13,18 +12,13 @@ def get_predictions():
 
     videos = []
 
-    new_videos_json = "new_videos.json"
-    if not os.path.exists(new_videos_json):
-        run_backend.update_db()
-
-    last_update = os.path.getmtime(new_videos_json) * 1e9
-
-    # if time.time_ns() - last_update > (720*3600*1e9):  # aprox. 1 mes
-    #    run_backend.update_db()
-
-    with open("new_videos.json", 'r') as data_file:
-        for line in data_file:
-            line_json = json.loads(line)
+    with sql.connect(run_backend.db_name) as conn:
+        c = conn.cursor()
+        for line in c.execute("SELECT * FROM videos"):
+            line_json = {"title": line[0],
+                         "video_id": line[1],
+                         "score": line[2],
+                         "update_time": line[3]}
             videos.append(line_json)
 
     predictions = []
@@ -32,25 +26,39 @@ def get_predictions():
         predictions.append((video['video_id'], video['title'],
                             float(video['score'])))
 
-    predictions = sorted(predictions, key=lambda x: x[2], reverse=True)[:20]
+    predictions = sorted(predictions, key=lambda x: x[2], reverse=True)[:25]
 
     predictions_formatted = []
     for e in predictions:
         predictions_formatted.append("<tr><th><a href=\"{link}\">{title}</a></th><th>{score}</th></tr>".format(title=e[1], link=e[0], score=e[2]))
 
-    return '\n'.join(predictions_formatted), last_update
+    return '\n'.join(predictions_formatted)
 
 
 @app.route('/')
 def main_page():
-    preds, last_update = get_predictions()
+    preds = get_predictions()
     return """<head><h1>Recomendador de Vídeos do Youtube</h1></head>
     <body>
-    Segundos desde a última atualização: {}
     <table>
              {}
     </table>
-    </body>""".format((time.time_ns() - last_update) / 1e9, preds)
+    </body>""".format(preds)
+
+
+@app.route('/predict')
+def predict_api():
+    yt_video_id = request.args.get("yt_video_id", default='')
+    video_page = get_data.download_video_page("/watch?v={}".format(yt_video_id))
+    video_json_data = get_data.parse_video_page(video_page)
+
+    if 'watch-time-text' not in video_json_data:
+        return "not found"
+
+    p = ml_utils.compute_prediction(video_json_data)
+    output = {"title": video_json_data['watch-title'], "score": p}
+
+    return json.dumps(output)
 
 
 if __name__ == '__main__':
