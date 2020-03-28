ae19d7f82751159b7ca2bbb6742aa83e24c8ec61
luis <luis.meazzini@gmail.com>
2020-03-28 18:13:34 -0300

    path updated

--- src/data_science/search_data_parsing.py
+++ src/data_science/search_data_parsing.py
@@ -12,7 +12,7 @@ number_of_pages = params['number_of_pages']
 
 for query in queries:
     for page in range(1, number_of_pages+1):
-        with open("../pages/{}_{}.html".format(query, page), 'r+') as inp:
+        with open("../../pages/{}_{}.html".format(query, page), 'r+') as inp:
 
             html_page = inp.read()
             parsed = bs.BeautifulSoup(html_page, features='html.parser')
@@ -22,7 +22,7 @@ for query in queries:
                 if tag.has_attr('aria-describedby'):
                     link = tag['href']
                     title = tag['title']
-                    with open('../data/parsed_videos.json', 'a+') as output:
+                    with open('../../data/parsed_videos.json', 'a+') as output:
                         data = {'link': link,
                                 'title': title,
                                 'query': query}
